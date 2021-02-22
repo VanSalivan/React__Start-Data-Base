e2960f3884b7361d9c861ef226b6afee9c65832e
aleksandr akelev <aleksandr.akelev@simbirsoft.com>
2021-02-23 01:15:03 +0300

    60. Логика индикатора загрузки

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -4,11 +4,12 @@ import Spiner from '../spiner/spiner';
 
 import './random-planet.scss';
 
-
+// Компонент для работы с состоянием и логикой
 export default class RandomPlanet extends Component {
     swapi = new SwapiService();
     state = {
         planet: {},
+        loading: true, // флаг показа спиннера : по умолчанию показывать - true
     };
 
     constructor() { // в момент создания компонента отправляем запрос на сервер
@@ -17,7 +18,10 @@ export default class RandomPlanet extends Component {
     };
 
     onPlanetLoaded = (planet) => { // стрелка для работы с значением this
-        this.setState({ planet }) // передаем в state обьект planet который приходит из this.swapi.getPlanet
+        this.setState({
+            planet, // передаем в state обьект planet который приходит из this.swapi.getPlanet
+            loading: false // снимаем флаг спинера в state как только данные готовы
+        })
     };
 
     updatePlanet() {
@@ -28,29 +32,41 @@ export default class RandomPlanet extends Component {
     };
 
     render() {
-
-        return (
+        // если мы загружаемася(true по умолчанию) : null(ничего)
+        const spiner = this.state.loading ? <Spiner /> : null;
+        // если не загружаемся/значит данные пришли показывает компонент с данными : null(ничего) - не отображает ничего
+        const content = !this.state.loading ? <PlanetViev planet={this.state.planet} /> : null;
+        return ( // null В JSX разметке игнорируется
             <div className="random-planet card" >
-                <img className="random-planet-image" src={`https://starwars-visualguide.com/assets/img/planets/${this.state.planet.id}.jpg`} alt="planet" />
-                <div className="card-body">
-                    <h4>{this.state.planet.planetName}</h4>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span className="term">Population</span>
-                            <span>{this.state.planet.population}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Rotation Period</span>
-                            <span>{this.state.planet.rotationPeriod}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Diameter</span>
-                            <span>{this.state.planet.diameter}</span>
-                        </li>
-                    </ul>
-                </div>
+                {spiner}
+                {content}
             </div>
         );
     }
 };
 
+// Компонент для отображения загруженых/полученных данных
+const PlanetViev = ({ planet }) => {
+    return (
+        <React.Fragment>
+            <img className="random-planet-image" src={`https://starwars-visualguide.com/assets/img/planets/${planet.id}.jpg`} alt="planet" />
+            <div className="card-body">
+                <h4>{planet.planetName}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{planet.population}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{planet.rotationPeriod}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{planet.diameter}</span>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
+    )
+};
